50e9f12f26c157025b36de0411013f9a70bdefa8
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-29 21:56:38 +0100

    Added function to get the request token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,8 @@ class ApplicationController < ActionController::API
         render status: status
     end
 
+    def current_token
+        return "" if !request.headers['Authorization'].present?
+        @token ||= request.headers['Authorization'].split(' ').last
+    end
 end
