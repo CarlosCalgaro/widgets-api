41449a1fb1ea8b76c1fd9f834485ee9d292ac751
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-29 21:55:35 +0100

    Added views and routes for authentication controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope :oauth do 
+    post '/token', to: "tokens#create", as: :authenticate
+    post '/revoke', to: 'tokens#revoke', as: :revoke
+    post '/refresh', to: 'authentications#refresh', as: :refresh
+  end
+
+
+
 end
