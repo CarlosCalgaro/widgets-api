7c13146e2a3c72a86d4ec7f1635c8c242cbcd611
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-29 21:56:15 +0100

    Added functions to handle controller responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+    
+    def render_success(status: :ok)
+        @message= I18n.t("request.success")
+        @code = ApiStatusCodes::SUCCESS
+        render status: status
+    end
+    
+    def render_error(status: :unprocessable_entity, message: "", code: ApiStatusCodes::ERROR)
+        @message = message.blank? ? I18n.t("request.error") : message
+        @code = code 
+        render status: status
+    end
+
 end
