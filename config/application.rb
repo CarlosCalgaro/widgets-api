898747f326cc083a98daf68ed07ed30e42081c8f
Carlos Calgaro <carloscalgarof@gmail.com>
2020-03-29 21:50:13 +0100

    Added Cors and JB Configuration

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,25 @@ module WidgetsApi
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
+
+
+    config.eager_load_paths << Rails.root.join('lib')
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+      end
+    end
+
+    config.generators do |g|
+      g.jbuilder false
+    end
+
     config.api_only = true
   end
 end
